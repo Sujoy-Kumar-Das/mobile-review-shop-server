4821a6d3911f8ffdb5c83f6da538b5a2dbec273c
sujoy-kumar-das <sujoykumardas75@gmail.com>
2023-04-04 18:29:29 +0600

    data for home api created

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 
 // middlewares
 app.use(cors())
-
+app.use(express.json());
 
 // database config
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_Password}@cluster0.j1u8ft3.mongodb.net/?retryWrites=true&w=majority`;
@@ -23,6 +23,32 @@ async function DBConnect(){
     }
 }
 DBConnect();
+
+// db collections
+const servicesCollections = client.db('Mobiel-dokan').collection('services')
+
+// get service for home 
+
+app.get('/home/products',async(req,res)=>{
+    try {
+        const query = {}
+        const currsor = servicesCollections.find(query).limit(3)
+        const services =  await currsor.toArray()
+        res.send({
+            success:true,
+            data:services
+        })
+        
+    } catch (error) {
+        res.send({
+            success:false,
+            data:[],
+            error:error
+        })
+        console.log(error)
+    }
+})
+
 
 
 
